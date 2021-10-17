b807d7cb254541e4cb78e0865e8e8d2ea29aa546
Dianing Yudono <dianing.yudono@shopify.com>
2021-10-17 16:26:42 +0100

    Add a test for plantsToCare query

--- houseplants/houseplants_api/tests.py
+++ houseplants/houseplants_api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
+from graphene.test import Client
 
-# Create your tests here.
+from schema import schema
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test_plants_to_care_query():
+    client = Client(schema)
+    executed = client.execute(
+        '''
+        query {
+            plantsToCare {
+                id
+                name
+                imageUrl
+                description
+            }
+        }
+        '''
+    )
+    assert executed == {
+        'data': {
+            'plantsToCare': []
+        }
+    }
