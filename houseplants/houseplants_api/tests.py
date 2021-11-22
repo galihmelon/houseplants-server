4f9f6cb727c91e0d3cf67349c749a094292ffdf8
Dianing Yudono <dianing.yudono@shopify.com>
2021-11-22 20:00:04 +0000

    Add plants need cleaning
    in plantsToCare query

--- houseplants/houseplants_api/tests.py
+++ houseplants/houseplants_api/tests.py
@@ -3,7 +3,7 @@ import pytest
 from graphene.test import Client
 
 from schema import schema
-from .test_factories import PlantFactory, WateringLogFactory
+from .test_factories import PlantFactory, CleaningLogFactory, WateringLogFactory
 
 
 pytestmark = pytest.mark.django_db
@@ -47,13 +47,18 @@ def test_plants_to_care_query_with_plants_and_no_logs():
         '''
     )
     plants_to_care = executed['data']['plantsToCare']
-    assert len(plants_to_care) == 1
+    # 1 result for watering and 1 result for cleaning
+    assert len(plants_to_care) == 2
 
 
 def test_plants_to_care_query_with_results():
-    plan_a = WateringLogFactory(next_suggested_date=date.today())
-    plan_b = WateringLogFactory(next_suggested_date=date.today() - timedelta(days=3))
-    plan_c = WateringLogFactory(next_suggested_date=date.today() + timedelta(days=3))
+    log_a = WateringLogFactory(next_suggested_date=date.today())
+    log_b = WateringLogFactory(next_suggested_date=date.today() - timedelta(days=3))
+    log_c = WateringLogFactory(next_suggested_date=date.today() + timedelta(days=3))
+
+    CleaningLogFactory(plant=log_a.plant, next_suggested_date=date.today())
+    CleaningLogFactory(plant=log_b.plant, next_suggested_date=date.today() - timedelta(days=3))
+    CleaningLogFactory(plant=log_c.plant, next_suggested_date=date.today() + timedelta(days=3))
 
     client = Client(schema)
     executed = client.execute(
@@ -69,10 +74,34 @@ def test_plants_to_care_query_with_results():
         '''
     )
     plants_to_care = executed['data']['plantsToCare']
-    assert len(plants_to_care) == 2
+    assert len(plants_to_care) == 4
 
     plant_ids = [int(plant['id']) for plant in plants_to_care]
-    assert plant_ids == [plan_a.plant.id, plan_b.plant.id]
+    assert set(plant_ids) == set([log_a.plant.id, log_b.plant.id])
+
+
+def test_water_plant_mutation():
+    PlantFactory(id=123)
+
+    client = Client(schema)
+    executed = client.execute(
+        '''
+        mutation {
+            waterPlant(plantId: 123) {
+                wateringLog {
+                    plant {
+                        id
+                    }
+                    nextSuggestedDate
+                    waterDate
+                }
+            }
+        }
+        '''
+    )
+    log = executed['data']['waterPlant']['wateringLog']
+    assert log['plant']['id'] == '123'
+    assert log['waterDate'] == date.today().strftime('%Y-%m-%d')
 
 
 def test_clean_plant_mutation():
@@ -97,27 +126,3 @@ def test_clean_plant_mutation():
     log = executed['data']['cleanPlant']['cleaningLog']
     assert log['plant']['id'] == '123'
     assert log['cleanDate'] == date.today().strftime('%Y-%m-%d')
-
-
-def test_water_plant_mutation():
-    PlantFactory(id=1)
-
-    client = Client(schema)
-    executed = client.execute(
-        '''
-        mutation {
-            waterPlant(plantId: 1) {
-                wateringLog {
-                    plant {
-                        id
-                    }
-                    nextSuggestedDate
-                    waterDate
-                }
-            }
-        }
-        '''
-    )
-    log = executed['data']['waterPlant']['wateringLog']
-    assert log['plant']['id'] == '1'
-    assert log['waterDate'] == date.today().strftime('%Y-%m-%d')
